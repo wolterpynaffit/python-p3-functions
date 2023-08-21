f6ae701c267ecb847a1aad79275b8b3baed66a96
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2023-08-21 16:18:06 -0400

    finished

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print('Hello programmer!')
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("Tiffany")
+    
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}")
+greet_with_default()
+    
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+add(3, 100)
+
 
 def halve(number):
-    pass
+    return number/2
+halve(43)
